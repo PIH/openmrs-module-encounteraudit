484c45d50ed4787c04033a29d6b135f50869afc8
Stephen Po-Chedley <pochedley@gmail.com>
2014-05-08 11:53:31 +0200

    initial commit

---
+++ api/src/main/java/org/openmrs/module/encounteraudit/api/impl/EncounterAuditServiceImpl.java
@@ -0,0 +1,44 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.encounteraudit.api.impl;
+
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.encounteraudit.api.EncounterAuditService;
+import org.openmrs.module.encounteraudit.api.db.EncounterAuditDAO;
+
+/**
+ * It is a default implementation of {@link EncounterAuditService}.
+ */
+public class EncounterAuditServiceImpl extends BaseOpenmrsService implements EncounterAuditService {
+	
+	protected final Log log = LogFactory.getLog(this.getClass());
+	
+	private EncounterAuditDAO dao;
+	
+	/**
+     * @param dao the dao to set
+     */
+    public void setDao(EncounterAuditDAO dao) {
+	    this.dao = dao;
+    }
+    
+    /**
+     * @return the dao
+     */
+    public EncounterAuditDAO getDao() {
+	    return dao;
+    }
+}
